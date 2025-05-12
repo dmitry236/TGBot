a70cf0bf8aefeb21fa2256af5017331e3d4c6fc8
dmitry236 <dementev.ru55@gmail.com>
2025-05-12 16:10:04 +0500

    Убрал админов

--- keyboards/all_kb.py
+++ keyboards/all_kb.py
@@ -1,11 +1,10 @@
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
-from create_bot import admins
 
 def main_kb(user_telegram_id: int):
     kb_list = [
         [KeyboardButton(text="📝 Удалить фон изображения"), KeyboardButton(text="📚 Создать стикерпак")]
     ]
-    if user_telegram_id in admins:
-        kb_list.append([KeyboardButton(text="⚙️ Админ панель")])
+    #if user_telegram_id in admins:
+    #    kb_list.append([KeyboardButton(text="⚙️ Админ панель")])
     keyboard = ReplyKeyboardMarkup(keyboard=kb_list, resize_keyboard=True, one_time_keyboard=True)
     return keyboard
